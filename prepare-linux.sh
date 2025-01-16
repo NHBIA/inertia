8b04079f4c88cb162d960ee5bc774e1fa980f3e4
Florian Mahon <florian.mahon@gmail.com>
2025-01-16 21:00:17 +0100

    ADD: Blinkled

--- prepare-linux.sh
+++ prepare-linux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rustup target add thumbv8m.main-none-eabihf
+rustup target add riscv32imac-unknown-none-elf
+
 git clone https://github.com/raspberrypi/pico-sdk-tools pico
 pushd pico
 ./build_linux.sh
